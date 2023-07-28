83f472bc5d45126a3e5ec5ab0394a412991fa44e
Chitra Janardhanan <chitrapaij@gmail.com>
2023-07-28 15:36:21 -0400

    completed client server connection

--- client/src/pages/tracks.tsx
+++ client/src/pages/tracks.tsx
@@ -1,12 +1,41 @@
 import React from "react";
 import { Layout } from "../components";
+import { gql } from "../__generated__/";
+import { useQuery } from "@apollo/client";
+import TrackCard from "../containers/track-card";
+import QueryResult from "../components/query-result";
+
+const TRACKS = gql(`
+query ExampleQuery {
+  tracksForHome {
+    id
+    title
+    author {
+      id
+      name
+      photo
+    }
+    thumbnail
+    length
+    modulesCount
+  }
+}
+`);
 
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  const { loading, error, data } = useQuery(TRACKS);
+  
+  return <Layout grid>
+  <QueryResult error={error} loading={loading} data={data}>
+  {data?.tracksForHome?.map((track) => (
+    <TrackCard key={track.id} track={track} />
+  ))}
+  </QueryResult>
+</Layout>
 };
 
 export default Tracks;
