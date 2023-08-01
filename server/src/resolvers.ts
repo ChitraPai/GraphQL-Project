7074267df5c10eac6b51ee3d7269daffa7c6afc6
Chitra Janardhanan <chitrapaij@gmail.com>
2023-08-01 04:21:13 -0400

    Implemented restdatasource and resolvers

---
+++ server/src/resolvers.ts
@@ -0,0 +1,17 @@
+import { Resolvers } from "./types";
+
+export const resolvers : Resolvers = {
+
+    Query: {
+        // returns an array of Tracks that will be used to populate
+        // the homepage grid of our web client
+        tracksForHome : (_,__,{dataSources}) => {
+            return dataSources.trackAPI.getTracksForHome();
+        },
+    },
+    Track: {
+        author : ({authorId},_,{dataSources}) =>{
+            return dataSources.trackAPI.getAuthor(authorId);
+        }
+    }
+};
